393f71f51631b1ac287794bdf380215a31366d84
Ahmed Naguib <ahmedmaelesawy@gmail.com>
2022-05-14 22:39:23 +0200

    Add Router

--- PlanRadarTask/Routers/Router.swift
+++ PlanRadarTask/Routers/Router.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+protocol PresentableAlert {
+    var sourceViewController: UIViewController? {get set}
+    func showAlert(title: String?, message: String)
+}
+
+extension PresentableAlert {
+    func showAlert(title: String?, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .cancel, handler: nil))
+        sourceViewController?.present(alert, animated: true, completion: nil)
+    }
+}
+
+protocol Presentable {
+    var sourceViewController: UIViewController? {get set}
+    func pushViewController(_ viewController: UIViewController)
+}
+extension Presentable {
+    func pushViewController(_ viewController: UIViewController) {
+        viewController.modalPresentationStyle = .fullScreen
+        sourceViewController?.navigationController?.pushViewController(viewController, animated: true)
+    }
+   
+}
+protocol Router: PresentableAlert, Presentable {}
+
--- PlanRadarTask/Routers/WeatherRouter.swift
+++ PlanRadarTask/Routers/WeatherRouter.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+protocol WeatherRouterProtocol: Router {
+    func pushWeatherDetails(weather: CityUIModel)
+    func pushWeatherHistory(weathers: [CityUIModel])
+}
+
+class WeatherRouter: WeatherRouterProtocol {
+    var sourceViewController: UIViewController?
+    
+    func pushWeatherDetails(weather: CityUIModel) {
+        let viewController = WeatherConfigurator.weatherDetails(weather: weather)
+        pushViewController(viewController)
+    }
+    
+    func pushWeatherHistory(weathers: [CityUIModel]) {
+        let viewController = WeatherConfigurator.weatherHistory(weathers: weathers)
+        pushViewController(viewController)
+    }
+}
